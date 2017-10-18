69dff91e4db5984e5b1af2a3c848b78e1a041a4a
stabrr <bobbystabile@yahoo.com>
2017-10-18 17:01:02 -0500

    Removed Console.logs for Rueben

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,34 @@
+//Initial variables for the hotdudes
+
 var hotDudes = ["Richard Gere", "David Duchovny", "Martin Freeman", "Rick Moranis"];
 
-
+//function to get the photos from the api and display
 function displayTopicPhoto() {
+  //grabs the topic name to get photos
 	var topic = $(this).attr("data-name");
-  console.log("this= "+topic);
+  //string to query api
 	var queryURL = "http://api.giphy.com/v1/gifs/search?q="+topic+"&api_key=dc6zaTOxFJmzC&limit=10";
 
+  //calls the api
   $.ajax({
     url: queryURL,
     method: "GET"
   })
 
-    //
+    //this will set the img with attributes for photo state and url for each state
     .done(function(response){
-      	console.log(response);
-
-        var imageStill = "" //response.data[0].images.original_still.url;
-      	var imageAnimate = ""//response.data[0].images.original.url;
+      	//initializes variables  
+        var imageStill = "" 
+      	var imageAnimate = ""
+        //set the default variables to still and the src url
         var state = "still"
         imageUrl = imageStill;
-
-      	console.log(imageUrl);
-        //var topicImage = $("<img");
-
-        //
+        //this will assign clear the element images and then update with 10 new images
         $("#images").empty();
         for (var i=0; i<response.data.length;i++)
         {
           imageStill=response.data[i].images.original_still.url;
           imageAnimate = response.data[i].images.original.url;
-          console.log("IS="+imageStill);
-          console.log("IA="+imageAnimate);
           topicImage = $("<img>");          //
           topicImage.attr("src", imageStill);
           topicImage.attr("data-still", imageStill);
@@ -39,7 +37,7 @@ function displayTopicPhoto() {
           topicImage.addClass("gifs");
           topicImage.attr("alt", topic);
           $("#images").prepend(topicImage);
-          console.log(topicImage);
+          
 
         }
 
@@ -48,62 +46,44 @@ function displayTopicPhoto() {
 
     })
 }      
-
+//function to have buttons show up at the top
 function renderButtons() {
-    // Deleting the topics prior to adding new topics
-        // (this is necessary otherwise you will have repeat buttons)
+  // Deleting the hotdues prior to adding new hotdudes
+  $("#buttons-view").empty();
+    // Looping through the array of hotdudes
+    for (var i = 0; i < hotDudes.length; i++) {
 
-        $("#buttons-view").empty();
-        console.log("hotDudes.length"+ hotDudes.length);
+      // create new buttons for each item in hot dudes
+      var a = $("<button>");
+      a.addClass("topics btn btn-default btn-success btn-group");
+      a.attr("data-name", hotDudes[i]);
+      a.text(hotDudes[i]);
+      $("#buttons-view").append(a);
 
-        // Looping through the array of topics
-        for (var i = 0; i < hotDudes.length; i++) {
+    };
+    
+}
 
-          // Then dynamicaly generating buttons for each topic in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-          var a = $("<button>");
-          // Adding a class of topics to our button
-          a.addClass("topics btn btn-default btn-success btn-group");
-          // Adding a data-attribute
-          a.attr("data-name", hotDudes[i]);
-          // Providing the initial button text
-          a.text(hotDudes[i]);
-          // Adding the button to the buttons-view div
-          $("#buttons-view").append(a);
-
-        };
-        console.log(a);
-      }
-
-// This function handles events where a movie button is clicked
+// This function handles events when a hot dude button is clicked
 $("#add-topic").on("click", function(event) {
     event.preventDefault();
-    console.log("event"+event);
     // This line grabs the input from the textbox
     var topics = $("#topic-input").val().trim();
     var alreadyUsed = $.inArray(topics,hotDudes);
-    console.log(topics);
-    console.log(alreadyUsed);
+    //checks to see if there is a blank or if it was already used
     if (topics && alreadyUsed<0){
-
-
-      // Adding topic from the textbox to our array
+      // Adding hotdude from the textbox to our array
       hotDudes.push(topics);
       $("#topic-input").val(" ");
-
-      // Calling renderButtons which handles the processing of our movie array
+      // Calling renderButtons which handles the processing of our hotdudes array
       renderButtons();
     }
 
-    });
-
-    // $(".gif").on("click", function() {
-        // STEP ONE: study the html above.
-        // Look at all the data attributes.
-        // Run the file in the browser. Look at the images.
-    function changePhotoState()   {
+});
+//function to check stat of photo and change to other state
+function changePhotoState()   {
         var state = $(this).attr("data-state");
-        console.log(state);
+        //checks state and then swtich src to other state and update state
         if (state =="still"){
           $(this).attr("data-state", "animate");
           $(this).attr("src",$(this).attr("data-animate"));
@@ -113,13 +93,12 @@ $("#add-topic").on("click", function(event) {
           $(this).attr("src",$(this).attr("data-still"));
         }
         
-    }
+}
 
 
 
-    // Adding a click event listener to all elements with a class of "movie"
- 	$(document).on("click", ".topics", displayTopicPhoto);
-  $(document).on("click", ".gifs", changePhotoState);
-
-    // Calling the renderButtons function to display the intial buttons
-    renderButtons();
+// Adding a click event listener to all elements with a class of "topics" and ".gifs"
+$(document).on("click", ".topics", displayTopicPhoto);
+$(document).on("click", ".gifs", changePhotoState);
+// Calling the renderButtons function to display the intial buttons
+renderButtons();
